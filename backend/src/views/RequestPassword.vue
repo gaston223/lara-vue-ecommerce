7ed57e8d0354d4bd8e9805d23905bad7319441b8
gaston223 <g.coulibalyy@gmail.com>
2022-12-27 20:34:54 +0100

    Request password and reset password views

--- backend/src/views/RequestPassword.vue
+++ backend/src/views/RequestPassword.vue
@@ -24,7 +24,7 @@
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Réinitialiser son mot de passe</h2>
             </div>
             <form class="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" value="true" />
@@ -33,20 +33,11 @@
                         <label for="email-address" class="sr-only">Email address</label>
                         <input id="email-address" name="email" type="email" autocomplete="email" required="" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
                     </div>
-                    <div>
-                        <label for="password" class="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autocomplete="current-password" required="" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
-                    </div>
                 </div>
 
                 <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-                    </div>
-
                     <div class="text-sm">
-                        <router-link :to="{name: 'requestPassword'}" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</router-link>
+                        <router-link :to="{name : 'login'}" class="font-medium text-indigo-600 hover:text-indigo-500">Revenir à la page d'accueil</router-link>
                     </div>
                 </div>
 
@@ -66,6 +57,10 @@
 <script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
 </script>
+
+<style scoped>
+
+</style>
 
 <style scoped>
 
